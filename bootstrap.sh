da05e32174842db6446b811e5582b483b9b5415a
Luca Clementi <luca.clementi@gmail.com>
2012-02-09 00:07:22 +0000

    Use $ROLLSROOT variable for referencing boostrap-function.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,17 +2,20 @@
 #
 # This file should remain OS independent
 #
-# $Id: bootstrap.sh,v 1.1 2012/01/28 00:57:51 nadya Exp $
+# $Id: bootstrap.sh,v 1.2 2012/02/09 00:07:22 clem Exp $
 #
 # @Copyright@
 # @Copyright@
 #
 # $Log: bootstrap.sh,v $
+# Revision 1.2  2012/02/09 00:07:22  clem
+# Use $ROLLSROOT variable for referencing boostrap-function.sh
+#
 # Revision 1.1  2012/01/28 00:57:51  nadya
 # initial
 #
 
-. ../etc/bootstrap-functions.sh
+. $ROLLSROOT/etc/bootstrap-functions.sh
 
 install cmake 
 install xmlrpc-c
