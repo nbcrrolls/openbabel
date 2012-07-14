5156a2e3816d4659302287f69c86fc05d71b368a
Nadya Williams <nadya@sdsc.edu>
2012-07-14 01:42:53 +0000

    add wrapper script

---
+++ src/openbabel/openbabel.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# wrapper script for openbabel web service
+
+flags=""
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -d)               
+             flags="$flags -d"
+             ;;
+        -h)               
+             flags="$flags -h"
+             ;;
+        -p)               
+             flags="$flags -p"
+             ;;
+        -c)               
+             flags="$flags -c"
+             ;;
+        -x)               
+             shift
+             flags="$flags $1"
+             ;;
+        -ifile)
+             shift
+             ifile=$1
+             ;;
+        -iformat)
+             shift
+             iformat=$1
+             ;;
+        -ofile)
+             shift
+             ofile=$1
+             ;;
+        -oformat)
+             shift
+             oformat=$1
+             ;;
+        *)   echo "Invalid $1 argument in $@"
+             exit 1
+    esac
+    shift
+done
+
+if test -z "$ifile"; then
+  echo "ERROR: missing input file"
+  exit 1
+fi
+
+if test ! -z "$ofile"; then
+  ofile="output"
+fi
+
+cmd="/opt/openbabel/bin/babel $iformat $ifile $oformat $ofile $flags"
+
+echo "Executing command: $cmd"
+$cmd
